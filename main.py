778b6389ec8a9b5be9f644b3104e66a3715034a2
Ali <psymastercode@gmail.com>
2023-04-20 03:38:45 +0330

    Using Typeing for functions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import redis
 import requests
 from celery import Celery
+from typing import List, Union, Tuple
 
 from dotenv import load_dotenv
 
@@ -11,14 +12,14 @@ app = Celery('main', backend='redis://localhost:6370', broker='redis://localhost
 app.config_from_object('schedules')
 
 
-def city_request(city):
+def city_request(city: str) -> Union[int, str]:
     redis_connection = redis.Redis(host="localhost", port=6370, db=0, decode_responses=True, charset='UTF-8')
     city_temp = redis_connection.get(f'temp_{city}')
     if city_temp is not None:
-        return city_temp
+        return int(city_temp)
     try:
         response = requests.get('https://api.openweathermap.org/data/2.5/weather',
-                       {"q": {city}, "appid": {os.environ.get('API_KEY')}})
+                                {"q": {city}, "appid": {os.environ.get('API_KEY')}})
         temp = int(response.json()['main']['temp']) - 273
         redis_connection.set(f'temp_{city}', temp, ex=60)
         return temp
@@ -27,10 +28,10 @@ def city_request(city):
 
 
 @app.task
-def some_cities(*args):
-    cities = ['tehran', 'joybar', 'gorgan', 'mashhad', 'tabriz',
-              'london', 'liverpool', 'madrid', 'munich', 'shiraz',
-              'ardebil']
+def some_cities(*args) -> List[Tuple[str, Union[int, str]]]:
+    cities: List[str] = ['tehran', 'joybar', 'gorgan', 'mashhad', 'tabriz',
+                         'london', 'liverpool', 'madrid', 'munich', 'shiraz',
+                         'ardebil']
 
-    mapping = list(zip(cities, list(map(city_request, cities))))
+    mapping: List[Tuple[str, Union[int, str]]] = [(city, city_request(city)) for city in cities]
     return mapping
